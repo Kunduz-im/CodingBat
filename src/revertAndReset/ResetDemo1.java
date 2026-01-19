679fe2d042654ecdb7d68028ea259a5d870578df
Kunduz-im <imankulovakndz03@gmail.com>
2026-01-19 10:04:43 +0600

    Update ResetDemo1.java

--- src/revertAndReset/ResetDemo1.java
+++ src/revertAndReset/ResetDemo1.java
@@ -3,6 +3,6 @@ package revertAndReset;
 public class ResetDemo1 {
     public static void main(String[] args) {
         System.out.println(" Reset one ");
-        System.out.println(Reset one );
+      
     }
 }
