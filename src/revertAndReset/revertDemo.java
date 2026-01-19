6540ce4082c3684d664793f8231651b4c7ae7893
Kunduz-im <imankulovakndz03@gmail.com>
2026-01-19 09:44:02 +0600

    Revert "My name"
    This reverts commit 94ea8af59d07a02fbae3b04481e50092a5b8ac9b.

--- src/revertAndReset/revertDemo.java
+++ src/revertAndReset/revertDemo.java
@@ -3,6 +3,5 @@ package revertAndReset;
 public class revertDemo {
     public static void main(String[] args) {
         System.out.println("First one");
-        System.out.println(My name is Ali);
     }
 }
